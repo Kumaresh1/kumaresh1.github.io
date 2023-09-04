635b50dae2fd5e98c9f16d877f4d4bee02d5d69d
Kumaresh <79594917+Kumaresh1@users.noreply.github.com>
2023-09-04 09:23:25 +0530

    PDF link updated

--- src/components/Splitleft.js
+++ src/components/Splitleft.js
@@ -1,57 +1,50 @@
-import React from 'react'
-import '../css/left.css'
-import proglogo from '../images/programmer.png'
+import React from 'react';
+import '../css/left.css';
+import proglogo from '../images/programmer.png';
 
-import axios from 'axios'
+import axios from 'axios';
 
-
-function postmessage(){
-  
-    axios.post('https://secretmsgs.herokuapp.com/portfolio/message', {
-  "message":prompt("Your Name ? ")
-})
-.then(function (response) {
-  
- // console.log(response);
-  document.getElementById("mbox").value="";
-  //alert("Message sent");
-
-})
+function postmessage() {
+    axios
+        .post('https://secretmsgs.herokuapp.com/portfolio/message', {
+            message: prompt('Your Name ? '),
+        })
+        .then(function (response) {
+            // console.log(response);
+            document.getElementById('mbox').value = '';
+            //alert("Message sent");
+        });
 }
 
 export default function Splitleft() {
     return (
         <div>
-
             <div className="bor-sl">
+                <div className="s-l">
+                    <h3 className="hello">HELLO</h3>
 
+                    <svg height="10">
+                        <line x1="0" y1="0" x2="500" y2="0" />
+                    </svg>
 
-            <div className="s-l">
-            
-        <h3 className="hello">HELLO</h3>
-        
-        <svg height="10">
-  <line x1="0" y1="0" x2="500" y2="0"   />
-</svg>
+                    <h1>I AM KUMARESH</h1>
 
-<h1>I AM KUMARESH</h1>
+                    <h4>SOFTWARE DEVELOPER</h4>
 
-<h4>SOFTWARE DEVELOPER</h4>
+                    <button className="ping" onClick={postmessage}>
+                        PING ME
+                    </button>
 
-<button className="ping" onClick={postmessage}>PING ME</button>
+                    <button className="info">
+                        <a href="Kumaresh_V_12.pdf" download>
+                            GET CV
+                        </a>
+                    </button>
+                    <img src={proglogo} alt="programmer" />
 
-<button className="info">
-    <a href="RESUME_KUMARESH.pdf" download>
-    GET CV</a></button>
-<img src={proglogo} alt="programmer" />
-
-<div id="about"></div>
+                    <div id="about"></div>
+                </div>
+            </div>
         </div>
-            </div>
-
-            </div>
-            
-
-
-    )
+    );
 }
